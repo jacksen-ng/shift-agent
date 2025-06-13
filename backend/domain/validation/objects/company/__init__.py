c27a705f55b667e4a578f85d5f23aa4906885662
jacksen-ng <ngjacksen04@gmail.com>
2025-06-13 16:36:00 +0900

    objects: company_table validationの定義

---
+++ backend/domain/validation/objects/company/__init__.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from company.company_id import CompanyID
+from company.company_name import CompanyName
+from company.store_locate import StoreLocate
+from company.open_time import OpenTime
+from company.close_time import CloseTime
+from company.target_sales import TargetSales
+from company.laber_cost import LaberCost
+
+TypeModels = {
+    'CompanyID': CompanyID,
+    'CompanyName': CompanyName,
+    'StoreLocate': StoreLocate,
+    'OpenTime': OpenTime,
+    'CloseTime': CloseTime,
+    'TargetSales': TargetSales,
+    'LaberCost': LaberCost
+}
