761b0db0a49b52e8520afed2f1e47ac053fbe1e8
jacksen-ng <ngjacksen04@gmail.com>
2025-06-14 22:22:57 +0900

    feat: add boolean guard type

--- backend/domain/validation/models/guard_types/__init__.py
+++ backend/domain/validation/models/guard_types/__init__.py
@@ -6,10 +6,12 @@ from guard_types.integer import IntegerType
 from guard_types.string import StringType
 from guard_types.date import DateType
 from guard_types.time import TimeType
+from guard_types.boolean import BooleanType
 
 TypeModels = {
     'IntegerType': IntegerType,
     'StringType': StringType,
     'DateType': DateType,
-    'TimeType': TimeType
+    'TimeType': TimeType,
+    'BooleanType': BooleanType
 }
