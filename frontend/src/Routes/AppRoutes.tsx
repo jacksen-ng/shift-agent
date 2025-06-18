5bb0d748a2bc5ef0cc3f388a5765dd966fac6343
s2322013 <s2322013@stu.musashino-u.ac.jp>
2025-06-18 21:35:19 +0900

    fix: 最初の画面をログインに変更し、ルート構成を整理

--- frontend/src/Routes/AppRoutes.tsx
+++ frontend/src/Routes/AppRoutes.tsx
@@ -1,45 +1,36 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../Features/Auth/Login';
 import HostRegister from '../Features/Auth/HostRegister';
 import HostHome from '../Features/Host/Pages/Home';
 import StoreInfo from '../Features/Host/Pages/StoreInfo';
 import CrewInfo from '../Features/Host/Pages/CrewInfo';
+import CrewAdd from '../Features/Host/Pages/CrewCreate';
+import CrewEdit from '../Features/Host/Pages/CrewEdit';
 import CrewHome from '../Features/Crew/Pages/CrewHome';
 import ShiftSubmit from '../Features/Crew/Pages/ShiftSubmit';
-import CrewAdd from '../Features/Host/Pages/CrewCreate';
-
 
 const AppRoutes = () => {
   return (
     <Routes>
+      {/* 🔰 最初のルート：ログイン画面にリダイレクト */}
+      <Route path="/" element={<Navigate to="/login" />} />
+
       {/* 共通認証系 */}
       <Route path="/login" element={<Login />} />
       <Route path="/register/host" element={<HostRegister />} />
-   
 
-
-
-     
-
-      {/* ホスト画面 */}
-      <Route path="/host/home" element={<HostHome />} />
-      {/* 他のホストルートもここに記載 */}
-
-      {/* クルー画面 */}
-      <Route path="/crew/home" element={<CrewHome />} />
       {/* ホスト用 */}
       <Route path="/host/home" element={<HostHome />} />
       <Route path="/host/store-info" element={<StoreInfo />} />
       <Route path="/host/crew-info" element={<CrewInfo />} />
-      <Route path="/host/crew/add" element={<CrewAdd />} /> 
+      <Route path="/host/crew/add" element={<CrewAdd />} />
+      <Route path="/host/crew-edit/:name" element={<CrewEdit />} />
 
       {/* クルー用 */}
-      <Route path="/crew/home" element={<CrewHome />} /> 
+      <Route path="/crew/home" element={<CrewHome />} />
       <Route path="/crew/shift-submit" element={<ShiftSubmit />} />
     </Routes>
   );
 };
 
 export default AppRoutes;
-
-
