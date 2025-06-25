05780889340e2f8a644e79a24eae72b29d92b1b6
jacksen-ng <ngjacksen04@gmail.com>
2025-06-25 21:30:45 +0900

    fix: auth0id

--- backend/domain/validation/objects/user/__init__.py
+++ backend/domain/validation/objects/user/__init__.py
@@ -4,14 +4,14 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from user.user_id import UserIDValidation
 from user.company_id import CompanyIDValidation
-from user.auth0_id import Auth0IDValidation
+from user.firebase_uid import FirebaseUIDValidation
 from user.email import EmailValidation
 from user.role import RoleValidation
 
 user_validation = {
     'UserIDValidation': UserIDValidation,
     'CompanyIDValidation': CompanyIDValidation,
-    'Auth0IDValidation': Auth0IDValidation,
+    'FirebaseUIDValidation': FirebaseUIDValidation,
     'EmailValidation': EmailValidation,
     'RoleValidation': RoleValidation
 }
