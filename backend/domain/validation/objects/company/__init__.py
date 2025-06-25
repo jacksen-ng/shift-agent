9e336390b47414638d26f9c0d26ca1d7f0d58445
Kanra_Ishido <kanra.work@gmail.com>
2025-06-25 20:47:21 +0900

    comanyのバリデーションを修正

--- backend/domain/validation/objects/company/__init__.py
+++ backend/domain/validation/objects/company/__init__.py
@@ -2,20 +2,20 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from company.company_id import CompanyID
-from company.company_name import CompanyName
-from company.store_locate import StoreLocate
-from company.open_time import OpenTime
-from company.close_time import CloseTime
-from company.target_sales import TargetSales
-from company.labor_cost import LaborCost
+from company.company_id import CompanyIDValidation
+from company.company_name import CompanyNameValidation
+from company.store_locate import StoreLocateValidation
+from company.open_time import OpenTimeValidation
+from company.close_time import CloseTimeValidation
+from company.target_sales import TargetSalesValidation
+from company.labor_cost import LaborCostValidation
 
-TypeModels = {
-    'CompanyID': CompanyID,
-    'CompanyName': CompanyName,
-    'StoreLocate': StoreLocate,
-    'OpenTime': OpenTime,
-    'CloseTime': CloseTime,
-    'TargetSales': TargetSales,
-    'LaberCost': LaborCost
+company_validation = {
+    'CompanyIDValidation': CompanyIDValidation,
+    'CompanyNameValidation': CompanyNameValidation,
+    'StoreLocateValidation': StoreLocateValidation,
+    'OpenTimeValidation': OpenTimeValidation,
+    'CloseTimeValidation': CloseTimeValidation,
+    'TargetSalesValidation': TargetSalesValidation,
+    'LaborCostValidation': LaborCostValidation
 }
--- backend/domain/validation/objects/company/target_sales.py
+++ backend/domain/validation/objects/company/target_sales.py
@@ -1,13 +1,14 @@
 import os
 import sys
-import time
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../models"))
-import guard_types
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from models.guard_types import type_models
 
-class TargetSales:
+class TargetSalesValidation:
     def __init__(self, value: int):
-        self.value = guard_types.IntegerType(value).execute()
+        self.value = value
     
     def execute(self):
-        return self.value
+        validated_value = type_models['IntegerType'](self.value).execute()
+        
+        return validated_value
