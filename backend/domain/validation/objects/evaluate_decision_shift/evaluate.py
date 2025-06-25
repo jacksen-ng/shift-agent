af5e8d2342d2f69ef67289abdda11c19d3b77899
jacksen-ng <ngjacksen04@gmail.com>
2025-06-25 21:17:34 +0900

    fix: evaluate-decision-shift validation

--- backend/domain/validation/objects/evaluate_decision_shift/evaluate.py
+++ backend/domain/validation/objects/evaluate_decision_shift/evaluate.py
@@ -2,13 +2,14 @@ import os
 import sys
 from typing_extensions import Literal
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../models"))
-import guard_types
-import rules
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+from models.guard_types import type_models
 
-class Evaluate:
+class EvaluateValidation:
     def __init__(self, value: Literal[1, 2, 3, 4, 5]):
-        self.value = rules.LiteralEvaluate(value).execute()
+        self.value = value
     
     def execute(self):
-        return self.value
+        validated_value = type_models['IntegerType'](self.value).execute()
+        
+        return validated_value
