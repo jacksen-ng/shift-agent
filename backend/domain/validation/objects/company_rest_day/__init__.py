5335de187a6565f9da26f43e7d9e174289b1f700
Kanra_Ishido <kanra.work@gmail.com>
2025-06-25 22:12:09 +0900

    company rest dayのバリデーションを修正

--- backend/domain/validation/objects/company_rest_day/__init__.py
+++ backend/domain/validation/objects/company_rest_day/__init__.py
@@ -2,13 +2,12 @@ import os
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from company_rest_day.company_rest_day_id import CompanyRestDayIDValidation
+from company_rest_day.company_id import CompanyIDValidation
+from company_rest_day.rest_day import RestDayValidation
 
-from company_rest_day.company_id import CompanyID
-from company_rest_day.company_rest_day_id import CompanyRestDayID
-from company_rest_day.rest_day import RestDay
-
-CompanyRestDayModels = {
-    'CompanyID': CompanyID,
-    'CompanyRestDayID': CompanyRestDayID,
-    'RestDay': RestDay
+company_rest_day_validation = {
+    'CompanyRestDayIDValidation': CompanyRestDayIDValidation,
+    'CompanyIDValidation': CompanyIDValidation,
+    'RestDayValidation': RestDayValidation
 }
