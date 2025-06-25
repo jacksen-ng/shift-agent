1448eed836631269c98d1d748a9832fff3ec9f25
jacksen-ng <ngjacksen04@gmail.com>
2025-06-25 20:32:34 +0900

    fix: firebase auth

--- backend/app/secret_manager/secret_key.py
+++ backend/app/secret_manager/secret_key.py
@@ -21,3 +21,9 @@ def get_cloudsql_secret(project_id, secret_id, version_id="latest"):
     name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(name=name)
     return response.payload.data.decode("UTF-8")
+
+def get_firebase_secret(project_id, secret_id, version_id="latest"):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+    response = client.access_secret_version(name=name)
+    return response.payload.data.decode("UTF-8")
