1c6c5584c4309e93370028e77b2ba573672814a6
Kanra_Ishido <kanra.work@gmail.com>
2025-06-27 04:13:20 +0900

    オーナーシフト調整のシフトデータ取得usecaseのみ用意

---
+++ backend/app/usecase/owner_shift/get_shift_info.py
@@ -0,0 +1,18 @@
+from ....domain.validation.objects.company import company_validation
+from ....domain.entity.owner_shift import owner_shift_entities
+from ...repository.crud.edit_shift import edit_shift_repository
+
+class GetShiftInfoUseCase:
+    def __init__(self, company_id):
+        self.company_id = company_id
+
+    def execute(self):
+        # 検証
+
+        company_id_validation = company_validation['CompanyIDValidation'](self.company_id).execute()
+
+        company_id_entity = owner_shift_entities['ShiftInfoEntity'](company_id_validation).to_json()
+
+        return_values = edit_shift_repository['edit_shift_request'](company_id_entity)
+
+        return return_values
