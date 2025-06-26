c065bc0499ea8d217070e71a663972b09375c0e1
Kanra_Ishido <kanra.work@gmail.com>
2025-06-26 19:29:45 +0900

    home_pageユースケースを修正

--- backend/app/usecase/home_page/get_decision_shift.py
+++ backend/app/usecase/home_page/get_decision_shift.py
@@ -12,6 +12,6 @@ class GetDecisionShiftUseCase:
         company_id_validation = company_validation['CompanyIDValidation'](self.company_id).execute()
         company_id_entity = home_page_entities['DecisionShiftEntity'](company_id_validation).to_json()
 
-        company_decision_shift = decision_shift_repository['decision_shift_request'](company_id_entity)
+        company_decision_shift = decision_shift_repository['decision_shift_request'](company_id_entity['company_id'])
 
         return company_decision_shift
