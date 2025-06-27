5426a5e75a6208337458d56edc1a1d35de8812c9
Kanra_Ishido <kanra.work@gmail.com>
2025-06-28 03:08:59 +0900

    クルーのシフト提出ルートを作成

---
+++ backend/routes/crew_shift.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, HTTPException, Request, Response
+from fastapi.responses import JSONResponse
+
+from ..app.usecase.crew_shift import crew_shift_usecases
+from ..app.service.auth import auth_services
+
+app = APIRouter()
+
+@app.post('/submitted-shift')
+def post_submit_shift(request: Request, response: Response, company_member_info, submit_shift):
+    try:
+        auth_services.verify_and_refresh_token(request, response, required_role="owner")
+
+        crew_shift_usecases['PostSubmittedShiftUseCase'](company_member_info['user_id'], company_member_info['company_id'], submit_shift)
+
+    except HTTPException as e:
+        return JSONResponse(status_code=401, content={'message': e})
+    
+    except Exception as e:
+        return JSONResponse(status_code=400, content={'message': e})
