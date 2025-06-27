addb891ad87b7caafb05ac7b71de0a80db5ac4c5
Kanra_Ishido <kanra.work@gmail.com>
2025-06-27 20:44:57 +0900

    ライブラリ読み取り修正

--- backend/routes/crew_info.py
+++ backend/routes/crew_info.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 
 from ..app.usecase.home_page import home_page_usecase
