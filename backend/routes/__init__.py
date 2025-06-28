9c27555faac7430cb8e1e1b7e77cc649f9639dea
jacksen-ng <ngjacksen04@gmail.com>
2025-06-28 11:20:21 +0900

    fix: Routerの問題およびレスポンスの修正

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,23 +1,17 @@
-from .auth import post_login, post_signin
-from .company_info import get_company_info, edit_company_info
-from .crew_info import get_crew_info, edit_crew_info
-from .crew_shift import post_submit_shift
-from .gemini import gemini_create_shift, gemini_evaluate_shift
-from .home_page import get_decision_shift
-from .owner_shift import get_shift_info, edit_shift, complete_shift
+from .auth import app as auth_router
+from .company_info import app as company_info_router
+from .crew_info import app as crew_info_router
+from .crew_shift import app as crew_shift_router
+from .gemini import app as gemini_router
+from .home_page import app as home_page_router
+from .owner_shift import app as owner_shift_router
 
-routes = {
-    'post_login': post_login,
-    'post_signin': post_signin,
-    'get_company_info': get_company_info,
-    'edit_company_info': edit_company_info,
-    'get_crew_info': get_crew_info,
-    'edit_crew_info': edit_crew_info,
-    'post_submit_shift': post_submit_shift,
-    'gemini_create_shift': gemini_create_shift,
-    'gemini_evaluate_shift': gemini_evaluate_shift,
-    'get_decision_shift': get_decision_shift,
-    'get_shift_info': get_shift_info,
-    'edit_shift': edit_shift,
-    'complete_shift': complete_shift
-}
+__all__ = [
+    "auth_router",
+    "company_info_router", 
+    "crew_info_router",
+    "crew_shift_router",
+    "gemini_router", 
+    "home_page_router",
+    "owner_shift_router"
+]
--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from fastapi.responses import JSONResponse
 
 from ..app.usecase.auth import auth_usecase
@@ -6,9 +6,9 @@ from ..app.usecase.auth import auth_usecase
 app = APIRouter()
 
 @app.post('/login')
-def post_login(email, password):
+def post_login(email, password, response: Response):
     try:
-        response_values = auth_usecase['LoginUsecase'](email, password).execute()
+        response_values = auth_usecase['LoginUsecase'](email, password).execute(response)
         return response_values
     except HTTPException as e:
         return JSONResponse(status_code=401, content={'message': e})
