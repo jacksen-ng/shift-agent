26b39d137e5da428f1368e560fb6f1f76384ad35
Takahashi-Kaisei <130371620+Takahashi-Kaisei@users.noreply.github.com>
2025-06-30 03:00:50 +0900

    fix: eval_shift_toolをeval_final_shift_toolに変更し、シフト評価処理を修正

--- backend/app/service/agent/tests/test_shift_creator_ver4.py
+++ backend/app/service/agent/tests/test_shift_creator_ver4.py
@@ -1,7 +1,7 @@
 import json
 import os
 from module.shift_creator import shift_creator_run
-from module.shift_creator import eval_shift_tool
+from module.shift_creator import eval_final_shift_tool
 
 # def load_dummy_input_json():
 #     with open("data/inputs/02_sample.json", "r", encoding="utf-8") as f:
@@ -10,11 +10,25 @@ from module.shift_creator import eval_shift_tool
 
 # dummy_data = load_dummy_input_json()
 
-shift_result = shift_creator_run(shift_request_path = "data/inputs/02_sample.json", numb_rate_revisions=3)
+# shift_result = shift_creator_run(shift_request_path = "data/inputs/02_sample.json", numb_rate_revisions=3)
 
 
-file_path = "data/outputs/final_shift.txt"
+# file_path = "data/outputs/final_shift.txt"
 
-with open(file_path, 'w', encoding='utf-8') as file:
-    file.write(shift_result)
-print("終わり！！！！！！")
+# with open(file_path, 'w', encoding='utf-8') as file:
+#     file.write(shift_result)
+# print("終わり！！！！！！")
+
+
+
+file_path = "data/inputs/05_evaluate-gemini-input.json"
+with open(file_path, 'r', encoding='utf-8') as file:
+    shift_data = file.read()
+
+
+print(shift_data)
+print("確定シフトが出力されました．")
+print("-----------------------")
+eval_shift = eval_final_shift_tool(shift_data)
+print(eval_shift)
+print("できた．")
